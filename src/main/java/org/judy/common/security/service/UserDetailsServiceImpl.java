3b3e2a0ecc4d4cce2e83825bbe3f35f22c3cbe6d
Potwings <ygk1995@naver.com>
2021-04-23 17:53:56 +0900

    judy

--- src/main/java/org/judy/common/security/service/UserDetailsServiceImpl.java
+++ src/main/java/org/judy/common/security/service/UserDetailsServiceImpl.java
@@ -3,35 +3,28 @@ package org.judy.common.security.service;
 import org.judy.common.security.domain.CustomUser;
 import org.judy.common.security.domain.MemberVO;
 import org.judy.common.security.mapper.MemberMapper;
-import org.judy.manager.domain.Manager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-
-
 import lombok.extern.log4j.Log4j;
 
 @Log4j
-public class CustomUserDetailsService implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService{
 
 	@Autowired
 	private MemberMapper mapper;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		log.warn("userName : "+username);
-		
-		Manager manager = mapper.managerRead(username);
-		log.warn(manager);
-		
-		MemberVO vo = mapper.read(username);
-		
-		log.warn(vo);
-		
-		return manager == null ? new CustomUser(vo) : new CustomUser(manager);
-	}
 
+		log.warn("userName : " + username);
+			
+		MemberVO member = mapper.read(username);
+		 
+		 
+		return new CustomUser(member);
+
+	}
 }
